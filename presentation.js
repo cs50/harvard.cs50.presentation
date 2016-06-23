196c4569996dd11da1a25f2dfd6523d6dd237982
Kareem Zidane <kzidane@users.noreply.github.com>
2016-06-24 01:29:44 +0200

    hides avatar, status bar, theme icon, and tree in presentation mode (#5)
    * hides file tree in presentation mode
    
    * hides avatar, status bar, theme icon, and tree
    
    this hides avatar, status bar, theme icon, and tree in presentation mode only.
    
    * tidies & uses API method to show/hide theme icon
    
    - provides a function, showComponents, that's used to show/hide
    components (e.g., tree, status bar, etc) as presentation is toggled.
    
    - uses harvard.cs50.theme's API method, showButton, to show/hide
    theme icon.
    
    - removes login from dependencies as problematic in offline version.
    shows/hides avatar using CSS via setStyleRule.
    
    * tidies & ensures showComponents is passed boolean
    
    - sorts imports vars alphabetically.
    - ensures boolean is passed to showComponets.
    - sets display CSS property of avatar to initial upon resetting.
    - renames presentationOn to presenting.

--- presentation.js
+++ presentation.js
@@ -1,30 +1,31 @@
 define(function(require, exports, module) {
     main.consumes = [
-        "layout", "menus", "Plugin", "preferences", "settings", "tree", "ui"
+        "ace.status", "c9.ide.cs50.theme", "layout", "menus", "panels",
+        "Plugin", "preferences", "settings", "tree", "ui"
     ];
     main.provides = ["c9.ide.cs50.presentation"];
     return main;
 
     function main(options, imports, register) {
-        var Plugin = imports.Plugin;
         var layout = imports.layout;
         var menus = imports.menus;
+        var panels = imports.panels;
+        var Plugin = imports.Plugin;
         var prefs = imports.preferences;
         var settings = imports.settings;
-        var tree = imports.tree;
+        var status = imports["ace.status"];
+        var theme = imports["c9.ide.cs50.theme"];
         var ui = imports.ui;
-        
+
         var plugin = new Plugin("CS50", main.consumes);
-        
-        var presentationOn = false;
+
+        var presenting = false;
         var menuItem = null;
-        
-        var treeRowHeights = null;
-        
+
         /**
          * swaps values of settings at path1 and path2. setting values are
          * assumed to be numeric.
-         * 
+         *
          * @param {string} path1 path of first setting
          * @param {string} path2 path of second setting
          */
@@ -33,37 +34,65 @@ define(function(require, exports, module) {
             settings.set(path2, settings.getNumber(path1));
             settings.set(path1, val);
         }
-        
+
+        /**
+         * Shows/hides components (e.g., tree, status bar, etc).
+         *
+         * @param {boolean} show show/hide flag.
+         */
+        function showComponents(show) {
+            if (typeof show !== "boolean")
+                return;
+
+            if (show) {
+                panels.activate("tree");
+                status.show();
+                // show avatar
+                ui.setStyleRule(".btnName", "display", "initial");
+            }
+            else {
+                panels.deactivate("tree");
+                status.hide();
+                // hide avatar
+                ui.setStyleRule(".btnName", "display", "none !important");
+            }
+
+           theme.showButton(show);
+        }
+
         /**
          * toggles presentation mode on or off.
-         * 
+         *
          * @param {boolean} [override] ideally used when loading/unloading to
          * force toggle presentation mode on/off without saving settings.
-         */ 
+         */
         function togglePresentationMode(override) {
             if (typeof override === "boolean") {
                 // handle unloading when presentation is on
-                if (!override && presentationOn) {
+                if (!override && presenting) {
                     // toggle off presentation mode for ace and terminal
                     updateEditors();
+
+                    showComponents(true);
                 }
-                if (override == presentationOn) {
+                if (override == presenting) {
                     return;
                 }
-                presentationOn = override;
-                updateTree();
+                presenting = override;
             }
             else {
-                presentationOn = !presentationOn;
-                settings.set("user/cs50/presentation/@on", presentationOn);
+                presenting = !presenting;
+                settings.set("user/cs50/presentation/@presenting", presenting);
                 updateEditors();
-                updateTree();
             }
-            
+
+            // hide components (e.g., tree) in presentation only
+            showComponents(!presenting);
+
             // ensure menu item is in sync with the current mode
-            menuItem.checked = presentationOn;
+            menuItem.checked = presenting;
         }
-        
+
         function updateEditors() {
             swapSettings(
                 "user/cs50/presentation/@editorFontSize",
@@ -74,25 +103,7 @@ define(function(require, exports, module) {
                 "user/terminal/@fontsize"
             );
         }
-                
-        function updateTree() {
-            tree.off("draw", updateTree);
-            if (!tree.container)
-                return tree.on("draw", updateTree);
-            if (presentationOn) {
-                tree.container.classList.add("presentation-tree");
-                tree.tree.renderer.model.rowHeight = treeRowHeights.presentation;
-                tree.tree.renderer.model.rowHeightInner = treeRowHeights.presentation;
-            }
-            else {
-                tree.container.classList.remove("presentation-tree");
-                tree.tree.renderer.model.rowHeightInner = treeRowHeights.default;
-                tree.tree.renderer.model.rowHeight = treeRowHeights.default;
-            }
 
-            tree.tree.renderer.updateFull();
-        }
-        
         plugin.on("load", function() {
             // add menu item to View menu
             menuItem = new ui.item({
@@ -100,42 +111,27 @@ define(function(require, exports, module) {
                 caption: "Presentation Mode",
                 onclick: togglePresentationMode
             });
-            
+
             menus.addItemByPath(
                 "View/PresentationDiv", new ui.divider(), 1, plugin
             );
             menus.addItemByPath("View/Presentation Mode", menuItem, 2, plugin);
-            
-            treeRowHeights = {
-                default: parseInt(
-                    ui.getStyleRule(".filetree .tree-row", "height"), 10
-                ) 
-                || 22,
-                presentation: 40
-            };
-            
-            // CSS for tree
-            ui.insertCss(
-                require('text!./style.css'), 
-                options.staticPrefix, 
-                plugin
-            );
-            
+
             // default settings
             settings.on("read", function() {
                 settings.setDefaults("user/cs50/presentation", [
-                    ["on", false],
+                    ["presenting", false],
                     ["editorFontSize", 20],
                     ["terminalFontSize", 20]
                 ]);
             });
-            
+
             settings.on("write", function() {
-                if (settings.getBool("user/cs50/presentation/@on") !== presentationOn) {
+                if (settings.getBool("user/cs50/presentation/@presenting") !== presenting) {
                     menus.click("View/Presentation Mode");
                 }
             });
-            
+
             // add toggle button to preferences
             prefs.add({
                "CS50" : {
@@ -144,30 +140,24 @@ define(function(require, exports, module) {
                         position: 10,
                         "Presentation Mode" : {
                             type: "checkbox",
-                            setting: "user/cs50/presentation/@on",
+                            setting: "user/cs50/presentation/@presenting",
                             position: 200
                         }
                     }
                 }
             }, plugin);
-            
-            togglePresentationMode(
-                settings.getBool("user/cs50/presentation/@on")
-            );
 
-            // update tree row height on theme change
-            layout.on("eachTheme", function() {
-                updateTree();
-            });
+            togglePresentationMode(
+                settings.getBool("user/cs50/presentation/@presenting")
+            );
         });
-        
+
         plugin.on("unload", function() {
             togglePresentationMode(false);
             menuItem = null;
-            treeRowHeights = null;
-            presentationOn = false;
+            presenting = false;
         });
-    
+
         register(null, {
             "c9.ide.cs50.presentation": plugin
         });
