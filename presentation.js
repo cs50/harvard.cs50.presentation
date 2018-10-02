546b113f5107b694838b3019ae83131dbfac59a8
Kareem Zidane <kzidane@cs50.harvard.edu>
2018-10-02 15:35:09 -0400

    es6

--- presentation.js
+++ presentation.js
@@ -1,23 +1,23 @@
 define(function(require, exports, module) {
     main.consumes = [
-        "ace.status", "layout", "menus", "Plugin", "settings", "tree", "ui"
+        "ace.status", "layout", "menus", "Plugin", "settings", "ui"
     ];
     main.provides = ["harvard.cs50.presentation"];
     return main;
 
     function main(options, imports, register) {
-        var layout = imports.layout;
-        var menus = imports.menus;
-        var Plugin = imports.Plugin;
-        var settings = imports.settings;
-        var status = imports["ace.status"];
-        var ui = imports.ui;
+        const layout = imports.layout;
+        const menus = imports.menus;
+        const Plugin = imports.Plugin;
+        const settings = imports.settings;
+        const status = imports["ace.status"];
+        const ui = imports.ui;
 
-        var plugin = new Plugin("CS50", main.consumes);
+        const plugin = new Plugin("CS50", main.consumes);
 
-        var presenting;
-        var menuItem = null;
-        var barExtras = null;
+        let presenting;
+        let menuItem = null;
+        let barExtras = null;
 
         const listeners = [];
 
@@ -30,28 +30,28 @@ define(function(require, exports, module) {
          */
         function addListener(listener) {
 
-            // ensure listener is a fuction and listener is registered at most once
+            // Ensure listener is a fuction and listener is registered at most once
             if (typeof(listener) !== "function" || listeners.indexOf(listener) > -1)
                 return;
 
-            // register listener
+            // Register listener
             listeners.push(listener);
 
-            // call listener if presenting was initialized
+            // Call listener if presenting was initialized
             if (presenting !== undefined)
                 listener(presenting);
         }
 
 
         /**
-         * swaps values of settings at path1 and path2. setting values are
+         * Swaps values of settings at path1 and path2. setting values are
          * assumed to be numeric.
          *
          * @param {string} path1 path of first setting
          * @param {string} path2 path of second setting
          */
         function swapSettings(path1, path2) {
-            var val = settings.getNumber(path2);
+            const val = settings.getNumber(path2);
             settings.set(path2, settings.getNumber(path1));
             settings.set(path1, val);
         }
@@ -67,23 +67,23 @@ define(function(require, exports, module) {
                 return;
 
             if (show) {
-                // show status bar
+                // Show status bar
                 status.show();
 
-                // show elements hidden from menu bar
+                // Show elements hidden from menu bar
                 barExtras.$ext.classList.remove("cs50-presentation");
             }
             else {
-                // hide status bar
+                // Hide status bar
                 status.hide();
 
-                // hide particular elements from menu bar
+                // Hide particular elements from menu bar
                 barExtras.$ext.classList.add("cs50-presentation");
             }
         }
 
         /**
-         * toggles presentation mode on or off.
+         * Toggles presentation mode on or off.
          *
          * @param {boolean} [override] ideally used when loading/unloading to
          * force toggle presentation mode on/off without saving settings.
@@ -91,13 +91,13 @@ define(function(require, exports, module) {
         function togglePresentationMode(override) {
             if (typeof override === "boolean") {
 
-                // handle unloading when presentation is on
+                // Handle unloading when presentation is on
                 if (!override && presenting) {
 
-                    // toggle off presentation mode for ace and terminal
+                    // Toggle off presentation mode for ace and terminal
                     updateEditors();
 
-                    // show components that were hidden on presenting
+                    // Show components that were hidden on presenting
                     toggleElements(true);
                 }
 
@@ -112,13 +112,13 @@ define(function(require, exports, module) {
                 updateEditors();
             }
 
-            // hide components that should be hidden on presenting
+            // Hide components that should be hidden on presenting
             toggleElements(!presenting);
 
-            // sync menu item
+            // Sync menu item
             menuItem.setAttribute("checked", presenting);
 
-            // notify registered listeners
+            // Notify registered listeners
             listeners.forEach(listener => listener(presenting));
         }
 
@@ -130,30 +130,31 @@ define(function(require, exports, module) {
                 "user/cs50/presentation/@editorFontSize",
                 "user/ace/@fontSize"
             );
+
             swapSettings(
                 "user/cs50/presentation/@terminalFontSize",
                 "user/terminal/@fontsize"
             );
         }
 
-        plugin.on("load", function() {
+        plugin.on("load", () => {
 
-            // add menu item to View menu
+            // Add menu item to View menu
             menuItem = new ui.item({
                 type: "check",
                 caption: "Presentation Mode",
                 onclick: togglePresentationMode
             });
 
-            // divider after "View/Less Comfortable"
+            // Divider after "View/Less Comfortable"
             menus.addItemByPath("View/~", new ui.divider(), 1, plugin);
             menus.addItemByPath("View/Presentation Mode", menuItem, 2, plugin);
 
-            // find stats button
+            // Find stats button
             barExtras = layout.findParent({ name: "preferences" });
 
-            // default settings
-            settings.on("read", function() {
+            // Default settings
+            settings.on("read", () => {
                 settings.setDefaults("user/cs50/presentation", [
                     ["presenting", false],
                     ["editorFontSize", 18],
@@ -161,7 +162,7 @@ define(function(require, exports, module) {
                 ]);
             });
 
-            settings.on("write", function() {
+            settings.on("write", () => {
                 if (settings.getBool("user/cs50/presentation/@presenting") !== presenting)
                     menus.click("View/Presentation Mode");
             });
@@ -173,7 +174,7 @@ define(function(require, exports, module) {
             );
         });
 
-        plugin.on("unload", function() {
+        plugin.on("unload", () => {
             togglePresentationMode(false);
             menuItem = null;
             barExtras = null;
